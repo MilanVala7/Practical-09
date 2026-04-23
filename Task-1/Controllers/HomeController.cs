defc39a01c3bf2e8bc3a4121a127e2ab9e3ce1b9
Milan Vala <valamilan2005@gmail.com>
2026-04-23 18:20:15 +0530

    Completed Test-1

---
+++ Task-1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+    }
+}
