1077b5950141a633910513575f2b5779ad156ffa
Milan Vala <valamilan2005@gmail.com>
2026-04-24 15:20:39 +0530

    Task-3 Completed

---
+++ Task-3.Tests/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Task_3.Controllers;
+
+namespace Task_3.Tests.Controllers
+{
+    [TestClass]
+    public class TestController
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            // Here we are initializing the HomeController
+            HomeController controller = new HomeController();
+
+            // Act
+            // Here we are calling the Index method of the HomeController and storing the result in a variable with type ViewResult
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            // Here we are checking that the result is not null
+            Assert.IsNotNull(result);
+
+            // Here we are checking the ViewBag.Message is equal to "Hello, World!"
+            Assert.AreEqual("Hello World!", result.ViewBag.Message);
+
+        }
+
+    }
+}
---
+++ Task-3/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Task_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+    }
+}
